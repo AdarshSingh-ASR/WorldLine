df211e90a2f8ea673ae1df823c28d4c4f7c36f45
AdarshSingh-ASR <adarshsingh8a33@gmail.com>
2026-08-18 19:03:36 +0530

    Revert "Add reproducible WORLDLINE hackathon demo video"
    This reverts commit 8c04a09fea17e37030a6ffaddbff3903ac3d6689.

--- video/src/root.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {Composition} from 'remotion';
-import {WorldlineDemo} from './worldline-demo';
-
-export const FPS = 30;
-export const DURATION = 137 * FPS;
-export const Root: React.FC = () => <Composition id="WorldlineDemo" component={WorldlineDemo} durationInFrames={DURATION} fps={FPS} width={1920} height={1080} />;
